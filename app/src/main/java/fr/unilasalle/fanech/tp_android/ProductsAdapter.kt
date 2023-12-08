a35294c8091a72afe14470253dc6a7d39e7b7d51
lucas.fanech <fanech.lucas@gmail.com>
2023-12-08 17:48:50 +0100

    details of product

--- app/src/main/java/fr/unilasalle/fanech/tp_android/ProductsAdapter.kt
+++ app/src/main/java/fr/unilasalle/fanech/tp_android/ProductsAdapter.kt
@@ -8,21 +8,37 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-class ProductsAdapter(private var productList : ArrayList<Product>) : RecyclerView.Adapter<ProductsAdapter.ViewHolder>(){
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+class ProductsAdapter(
+    private var productList: ArrayList<Product>,
+    private val listener: OnClickListener
+) : RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
+
+    class ViewHolder(
+        itemView: View,
+        private val listener: OnClickListener
+    ) : RecyclerView.ViewHolder(itemView) {
         val title: TextView = itemView.findViewById(R.id.productTitle)
         val price: TextView = itemView.findViewById(R.id.productPrice)
         val image: ImageView = itemView.findViewById(R.id.productImage)
         fun bind(item: Product) {
             title.text = item.title
-            price.text = item.price.toString()+"€"
+            price.text = item.price.toString() + "€"
             Glide.with(itemView.context).load(item.image).into(image)
+            itemView.setOnClickListener {
+                listener.onClick(item)
+            }
         }
     }
 
+
+    interface OnClickListener {
+        fun onClick(position: Product)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.products_layout, parent, false)
-        return ViewHolder(view)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.products_layout, parent, false)
+        return ViewHolder(view, listener)
     }
 
     override fun getItemCount(): Int {
@@ -32,5 +48,11 @@ class ProductsAdapter(private var productList : ArrayList<Product>) : RecyclerVi
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = productList[position]
         holder.bind(item)
+
+        fun onClick(position: Product) {
+            holder.itemView.setOnClickListener {
+
+            }
+        }
     }
 }
