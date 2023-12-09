0a958e2266f4828665af5dd0f5604797e0665bc7
lucas.fanech <fanech.lucas@gmail.com>
2023-12-09 16:04:29 +0100

    Categories front + details front fixes

--- app/src/main/java/fr/unilasalle/fanech/tp_android/MainActivity.kt
+++ app/src/main/java/fr/unilasalle/fanech/tp_android/MainActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AlertDialog
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
@@ -21,6 +23,7 @@ class MainActivity : AppCompatActivity() , ProductsAdapter.OnClickListener
     private lateinit var productsAdapter: ProductsAdapter
     private val client = OkHttpClient()
     private var productList = ArrayList<Product>()
+    private var categoryList = ArrayList<String>()
     private var cartList    = ArrayList<Product>()
     private lateinit var viewModel: RetrofitViewModel
 
@@ -33,7 +36,19 @@ class MainActivity : AppCompatActivity() , ProductsAdapter.OnClickListener
         setContentView(binding.root) // R.layout.activity_main
 
 
-        productList.add(Product(1,"Album C'est pas des LOL",9.99f,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnDwLhK-z_u-lD0AUlHyeKZ5Wgb5yn6s6_fg&usqp=CAU","Meilleur album du J","test",Rating(1.0f,1)))
+        productList.add(
+            Product(
+                1,
+                "Album C'est pas des LOL",
+                9.99f,
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnDwLhK-z_u-lD0AUlHyeKZ5Wgb5yn6s6_fg&usqp=CAU",
+                "Meilleur album du J",
+                "test",
+                Rating(1.0f, 1)
+            )
+        )
+
+        categoryList.add("All categories")
 
         viewModel = RetrofitViewModel(RetrofitApi.getService())
         viewModel.products.observe(this) {
@@ -44,10 +59,17 @@ class MainActivity : AppCompatActivity() , ProductsAdapter.OnClickListener
             } else {
                 print("error")
             }
-
-
-
             productsAdapter.notifyDataSetChanged()
+        }
+
+        viewModel.categories.observe(this) {
+            if (it != null) {
+                for (category in it) {
+                    categoryList.add(category)
+                }
+            } else {
+                print("error")
+            }
         }
         // Products recyclerView
         val recyclerViewVariable = binding.productsRecyclerView
@@ -59,13 +81,23 @@ class MainActivity : AppCompatActivity() , ProductsAdapter.OnClickListener
             adapter = productsAdapter
         }
 
-
+        // category spinner
+        val categorySpinner: Spinner = findViewById(R.id.categoriesSpinner)
+        // Create an ArrayAdapter using the category list and a default spinner layout
+        ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            categoryList
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            categorySpinner.adapter = adapter
+        }
 
 
 
 
     }
-
     override fun oncClickAddToCart(position: Product) {
         cartList.add(position)
         // Pop up message
